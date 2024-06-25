ed68ccb544d48c73e0da957d98d5b46c68dd6ac2
kumarithakur4neha <nehakumari0673949@gmail.com>
2024-06-25 18:38:59 +0530

    mongo

--- models/connect.js
+++ models/connect.js
@@ -2,4 +2,4 @@ const mongoose=require("mongoose")
 
 require('dotenv').config();
 
-mongoose.connect(process.env.mongo_url).then(()=> console.log("db is established") ).catch((error)=>console.log(error.message));
+mongoose.connect("mongodb+srv://nehakumari0673949:neha444@cluster0.2coyw3n.mongodb.net/socialmedia?retryWrites=true&w=majority&appName=Cluster0").then(()=> console.log("db is established") ).catch((error)=>console.log(error.message));
